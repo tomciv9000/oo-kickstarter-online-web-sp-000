973437378e4cf1f0fae8e04f3dc49f2025535dc9
Tom White <eugene.white@gmail.com>
2019-06-12 23:01:39 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Project
   
   attr_reader :title, :backers
@@ -9,6 +11,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
+    binding.pry
   end
   
 end
